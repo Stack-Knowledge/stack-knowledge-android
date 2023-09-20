95736482b4357abdb253d74d274f1a6880eb9c73
채종인 <105656208+Chaejongin12@users.noreply.github.com>
2023-09-20 20:02:00 +0900

    Revert "🔀 :: (#3) Android Login Page 구현"

--- data/src/main/java/com/kdn/data/remote/dto/auth/request/RefreshTokenRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.kdn.data.remote.dto.auth.request
-
-data class RefreshTokenRequest(
-    val refreshToken: String,
-)
--- domain/src/main/java/com/kdn/domain/utils/Result.kt
+++
@@ -1,8 +0,0 @@
-package com.kdn.domain.utils
-
-sealed class Result<out T>{
-    object Loading : Result<Nothing>()
-    object UnLoading : Result<Nothing>()
-    data class Success<T>(val data: T) : Result<T>()
-    data class Unauthorized(val throwable: Throwable) : Result<Nothing>()
-}
