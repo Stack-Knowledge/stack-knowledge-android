e1e01b4f825851ac7d794578efd52893f8084213
Chaejongin12 <s22055@gsm.hs.kr>
2023-09-22 21:15:51 +0900

    :memo: ::  Ranking Item 작성
    ::  Ranking Item 작성

--- domain/src/main/java/com/kdn/domain/model/response/RankingResponseModel.kt
+++ domain/src/main/java/com/kdn/domain/model/response/RankingResponseModel.kt
@@ -1,5 +1,22 @@
 package com.kdn.domain.model.response
 
-data class RankingResponseModel (
-    val
-)
+import java.util.UUID
+
+data class RankingResponseModel(
+    val id: UUID,
+    val cumulatePoint: Int,
+    val user: User,
+) {
+    data class User(
+        val id: UUID,
+        val email: String,
+        val name: String,
+        val profileImage: String?,
+        )
+    fun RankingResponseModel.User.toEntity() = RankingResponseModel.User(
+        id = id,
+        email = email,
+        name = name,
+        profileImage = profileImage,
+    )
+}
--- presentation/src/main/java/com/kdn/stack_knowledge_android/adapter/RankingListAdapter.kt
+++ presentation/src/main/java/com/kdn/stack_knowledge_android/adapter/RankingListAdapter.kt
@@ -1,6 +1,27 @@
 package com.kdn.stack_knowledge_android.adapter
 
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import com.kdn.domain.model.response.RankingResponseModel
+import com.kdn.stack_knowledge_android.adapter.MissionListAdapter.Companion.diffUtil
 
-class RankingListAdapter(private val rankingList: List<RankingResponseModel>) {
+class RankingListAdapter(private val rankingList: List<RankingResponseModel>?) :
+    ListAdapter<RankingResponseModel, RankingListAdapter.RankingListViewHolder(diffUtil) {
+    class RankingListViewHolder(
+        val context: Context,
+        private val binding: Item
+    )
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RankingListAdapter.RankingListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RankingListAdapter.RankingListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
