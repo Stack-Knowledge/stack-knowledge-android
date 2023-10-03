a2d59e927dae9a668553f4d664b88184d768fb05
Chaejongin12 <s22055@gsm.hs.kr>
2023-10-04 00:36:50 +0900

    :memo: :: Interceptor 수정
    :: Interceptor 수정

--- data/src/main/java/com/kdn/data/remote/api/GoodsAPI.kt
+++ data/src/main/java/com/kdn/data/remote/api/GoodsAPI.kt
@@ -1,15 +1,12 @@
 package com.kdn.data.remote.api
 
 import com.kdn.data.remote.dto.goods.response.GoodsResponse
-import com.kdn.domain.entity.GoodsEntity
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Query
 
 
 interface GoodsAPI {
 
     @GET("/item")
     suspend fun getGoodsList(
-    ): List<GoodsEntity>
+    ): List<GoodsResponse>
 }
--- data/src/main/java/com/kdn/data/remote/datasource/goods/RemoteGoodsDataSourceImpl.kt
+++ data/src/main/java/com/kdn/data/remote/datasource/goods/RemoteGoodsDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.kdn.data.remote.datasource.goods
 
 import com.kdn.data.remote.api.GoodsAPI
+import com.kdn.data.remote.dto.goods.response.GoodsResponse
 import com.kdn.data.remote.dto.goods.response.toEntity
 import com.kdn.domain.entity.GoodsEntity
 import javax.inject.Inject
@@ -8,10 +9,8 @@ import javax.inject.Inject
 class RemoteGoodsDataSourceImpl @Inject constructor(
     private val goodsAPI: GoodsAPI,
 ) : RemoteGoodsDataSource {
-    override suspend fun getGoodsList(): List<GoodsEntity> =
-        goodsAPI.getGoodsList().map {
-            it.toEntity()
-        }
+    override suspend fun getGoodsList(): List<GoodsResponse> =
+        goodsAPI.getGoodsList()
 
 
 }
