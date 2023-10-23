76abdb9e8048f8dd295cc11f674b1f049adcff35
Chaejongin12 <s22055@gsm.hs.kr>
2023-10-23 22:32:57 +0900

    :sparkles: :: navgraph 분리 및 create mission xml 수정
    :: navgraph 분리 및 create mission xml 수정

---
+++ presentation/src/main/java/com/kdn/stack_knowledge_android/ui/mission/CreateMissionFragment.kt
@@ -0,0 +1,19 @@
+package com.kdn.stack_knowledge_android.ui.mission
+
+import androidx.fragment.app.activityViewModels
+import com.kdn.stack_knowledge_android.R
+import com.kdn.stack_knowledge_android.databinding.FragmentCreateMissionBinding
+import com.kdn.stack_knowledge_android.ui.base.BaseFragment
+import com.kdn.stack_knowledge_android.viewmodel.mission.MissionViewModel
+
+class CreateMissionFragment : BaseFragment<FragmentCreateMissionBinding>(R.layout.fragment_create_mission) {
+    private val missionViewModel by activityViewModels<MissionViewModel>()
+
+    override fun createView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
+    }
+}
