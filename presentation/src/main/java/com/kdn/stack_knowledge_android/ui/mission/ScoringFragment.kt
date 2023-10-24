ad13161f399e7496908ee1f7f0afd43118e0a7ed
Chaejongin12 <s22055@gsm.hs.kr>
2023-10-24 11:06:27 +0900

    :memo: AnswerFragment RecyclerView Item 수정
    answerFragment RecyclerView Item 수정

---
+++ presentation/src/main/java/com/kdn/stack_knowledge_android/ui/mission/ScoringFragment.kt
@@ -0,0 +1,16 @@
+package com.kdn.stack_knowledge_android.ui.mission
+
+import com.kdn.stack_knowledge_android.R
+import com.kdn.stack_knowledge_android.databinding.FragmentScoringBinding
+import com.kdn.stack_knowledge_android.ui.base.BaseFragment
+
+class ScoringFragment : BaseFragment<FragmentScoringBinding>(R.layout.fragment_scoring) {
+    override fun createView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeEvent() {
+        TODO("Not yet implemented")
+    }
+
+}
