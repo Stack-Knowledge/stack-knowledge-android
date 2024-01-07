1b092f8ef901d205cd830aa25d1e9246ef6d991e
Chaejongin12 <s22055@gsm.hs.kr>
2024-01-07 16:27:24 +0900

    :sparkles: :: AuthAPI logout suspend fun 세팅
    :: AuthAPI logout suspend fun 세팅

--- data/src/main/java/com/kdn/data/remote/api/auth/AuthAPI.kt
+++ data/src/main/java/com/kdn/data/remote/api/auth/AuthAPI.kt
@@ -4,6 +4,7 @@ import com.kdn.data.remote.dto.auth.request.GAuthLoginRequest
 import com.kdn.data.remote.dto.auth.response.GAuthLoginResponse
 import com.kdn.data.remote.dto.auth.response.RefreshTokenResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.Header
 import retrofit2.http.PATCH
 import retrofit2.http.POST
@@ -19,4 +20,9 @@ interface AuthAPI {
     suspend fun provideAccessToken(
         @Header ("refreshToken") refreshToken: String,
     ): RefreshTokenResponse
+
+    @DELETE("/auth")
+    suspend fun logout(
+        @Header("refreshToken") refreshToken: String,
+    )
 }
